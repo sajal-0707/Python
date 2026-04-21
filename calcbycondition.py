0c7502818c471a2ea1172116d6910ecc12246f25
Sajal Chourasia <sajal007@adsl178.16.0.13.manx.net>
2026-04-21 21:21:45 +0530

    Simple calculator using conditions

---
+++ calcbycondition.py
@@ -0,0 +1,15 @@
+a=int(input("Enter a number="))
+b=int(input("Enter a number="))
+ch=input("choose from (+,-,x,/) =")
+if ch=="+":
+    c=a+b
+    print(c)
+elif ch=="-":
+    c=a-b
+    print(c)
+elif ch=="*":
+    c=a*b
+    print(c)
+elif ch=="/":
+    c=a/b
+    print(c)
