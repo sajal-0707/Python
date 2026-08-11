d4473f10403f53e88ca6f70711910d2cba4ddad0
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-08-11 15:25:38 +0530

    Dicnoary in python

--- dict.py
+++ dict.py
@@ -1,6 +1,11 @@
 # s = {"name": "Alice", "age": 10, "grade": "A"}
 # print("Student name is",s["name"],"student age is",s["age"])
-car={'Brand': "ford", 'model': "mustang", 'year': "1967"}
-car['year']=1969
-car['color']="Black"
-print(car)
+# car={'Brand': "ford", 'model': "mustang", 'year': "1967"}
+# car['year']=1969
+# car['color']="Black"
+# print(car)
+dict={'apples':20,'banana':30,'mango':40}
+# print(list(dict.keys()))
+# print(list(dict.values()))
+print('apples' in dict)
+del dict['apples']
