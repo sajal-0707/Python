0d509462e040aed1c6e1481fac663db4059125da
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-08-11 09:47:34 +0530

    Operations on a dict

---
+++ dict.py
@@ -0,0 +1,6 @@
+# s = {"name": "Alice", "age": 10, "grade": "A"}
+# print("Student name is",s["name"],"student age is",s["age"])
+car={'Brand': "ford", 'model': "mustang", 'year': "1967"}
+car['year']=1969
+car['color']="Black"
+print(car)
